2b9c42f81bc87bce7561509b2d0116f69768e9c7
JW-1126 <jwyoon1126@naver.com>
2025-11-03 05:30:31 +0900

    refactor(repository): 저장소 컬렉션 Set -> List 변경

--- src/main/java/lotto/repository/Lottos.java
+++ src/main/java/lotto/repository/Lottos.java
@@ -1,19 +1,20 @@
 package lotto.repository;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.Lotto;
 
 public class Lottos {
 
-    private final Set<Lotto> lottos = new HashSet<>();
+    //동일한 숫자를 가진 로또들이 존재할 수도 있으므로 Set -> List 로 컬렉션 변경
+    private final List<Lotto> lottos = new ArrayList<>();
 
     public void add(Lotto lotto) {
         lottos.add(lotto);
     }
 
-    public HashSet<Lotto> getLottos() {
-        return new HashSet<>(lottos);
+    public List<Lotto> getLottos() {
+        return new ArrayList<>(lottos);
     }
 
 }
