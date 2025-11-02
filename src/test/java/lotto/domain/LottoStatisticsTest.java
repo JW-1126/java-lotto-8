ea1a739678e71feeee8ebeea574a177ea7a91628
JW-1126 <jwyoon1126@naver.com>
2025-11-03 05:58:32 +0900

    test: 당첨금 합계 계산 테스트 작성

--- src/test/java/lotto/domain/LottoStatisticsTest.java
+++ src/test/java/lotto/domain/LottoStatisticsTest.java
@@ -22,4 +22,20 @@ class LottoStatisticsTest {
         assertThat(lottoStatistics.getWinCount(1)).isEqualTo(1);
         assertThat(lottoStatistics.getWinCount(2)).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("각 순위의 당첨 갯수와 상금을 계산하여 합을 반환한다.")
+    void totalReward_ReturnSum() {
+        //given
+        LottoStatistics lottoStatistics = new LottoStatistics();
+
+        //when
+        lottoStatistics.addWinCount(1); //       50,000
+        lottoStatistics.addWinCount(2); //    1,500,000
+        lottoStatistics.addWinCount(2); //    1,500,000
+        lottoStatistics.addWinCount(3); //   30,000,000
+        lottoStatistics.addWinCount(4); //2,000,000,000
+
+        assertThat(lottoStatistics.totalReward()).isEqualTo(2033050000);
+    }
 }
