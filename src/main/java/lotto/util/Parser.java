81b63afb991ce20b5fc5a76e77fbc0a8aa9f84f8
JW-1126 <jwyoon1126@naver.com>
2025-11-02 17:24:44 +0900

    feat(parser): 입력값 파싱 기능 추가

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
-    public static List<String> inputParser(String input) {
+    public static List<String> inputToDivide(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
+                .toList();
+    }
+
+    public static List<Integer> wordToNumbers(List<String> input) {
+        return input.stream()
+                .map(Integer::parseInt)
                 .toList();
     }
 }
